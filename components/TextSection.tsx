cd801e1b0bf8fd6723cfaa79f820d29309a35953
gabrielTor <gabriel_torres2012@yahoo.com>
2023-03-17 19:52:56 -0300

    brief about section on homepage.

---
+++ components/TextSection.tsx
@@ -0,0 +1,26 @@
+import styles from '../styles/TextSection.module.css'
+
+export default function TextSection() {
+    return (
+        <section className={styles.container}>
+            <div>
+                <h1>Carpinteria Torres</h1>
+                <h3>Carpinteria Personalizada y a medida.</h3>
+                <p>Is a family owned and operated custom-woodwork corporation located in Miami,
+                    Florida. For many generations superior craftsmanship has been a tradition in the
+                    Martinez Family, our family. Today, Roberto & Robeisy Martinez <em>(3rd and 4th
+                        Generations carpenters)</em> proudly offers some of the finest handcrafted
+                    woodwork. For several years Robert Carpentry has provided its services to a
+                    wide variety of Luxury Residences, Hotels & Condominium, Restaurants, Stores
+                    and many more customers throughout South Florida and the Caribbean.</p>
+            </div>
+            <div>
+                <h4>WHATSAPP</h4>
+                <p>261-557-8851</p>
+                <h4>DIRECCION</h4>
+                <p>Peru 1264, Las Heras, Mendoza, CP5539</p>
+                <button className='button'>Contactar</button>
+            </div>
+        </section>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import img1 from '../assets/HomeSlider_Web_1.jpg'
 import img2 from '../assets/HomeSlider_Web_2.jpg'
 import img3 from '../assets/HomeSlider_Web_3.jpg'
 import img4 from '../assets/HomeSlider_Web_4.jpg'
+import TextSection from '../components/TextSection'
 
 const sliderArray = [img1, img2, img3, img4]
 const sliderContent = sliderArray.map((a, i) => ({ id: ++i, src: a }))
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Slider content={sliderContent} />
+      <TextSection />
     </>
   )
 }

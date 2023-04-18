4d88895586621340651b3aa2cfd0cf259e54aee7
gabrielTor <gabriel_torres2012@yahoo.com>
2023-04-18 20:33:52 -0300

    nav mobile x btn fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import styles from '../styles/Nav.module.css'
 import logo from '../assets/carpentry-logo.png'
 import { RxHamburgerMenu } from 'react-icons/rx'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const navArray = [
     { name: 'Inicio', id: 123, link: '/', isImage: false },
@@ -17,18 +17,27 @@ const showHomeWebLogo = (isString: any) => typeof isString === 'string'
 
 export default function Navbar() {
     const [active, setActive] = useState<boolean>(false)
+    const [web, setWeb] = useState<boolean>(true)
+
+    useEffect(() => window.outerWidth < 750 ? setWeb(false) : setWeb(true), [])
+
+    const handleActive = () => {
+        if (web) return
+        setActive(prev => !prev)
+    }
+
     return (
         <nav className={active ? styles.active : styles.navContainer}>
             <ul className={styles.navbar}>
                 {navArray.map(item => (
                     <li className={showHomeWebLogo(item.name) ? styles.navItem : `${styles.logo} ${styles.navItem}`} key={item.id}>
-                        <Link onClick={() => setActive(prev => !prev)} href={item.link}>
+                        <Link onClick={handleActive} href={item.link}>
                             {!item.isImage ? item.name as string : <Image src={item.name} alt='logo' width={150} height={150} priority />}
                         </Link>
                     </li>
                 ))}
             </ul>
-            <button onClick={() => setActive(prev => !prev)}>
+            <button onClick={handleActive}>
                 {active ? 'X' : <RxHamburgerMenu size={25} />}
             </button>
         </nav>

85e1f8609ba2332193aa41a811b5c7ebcaf83f05
gabrielTor <gabriel_torres2012@yahoo.com>
2023-04-18 23:38:26 -0300

    ignore undefined map types

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -5,9 +5,9 @@ import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import styles from '../../styles/Contact.module.css'
 
 export default function Map({ marker, popupText }: { marker: number[], popupText: string }) {
-    return (
+    return (//@ts-ignore
         <MapContainer className={styles.map} center={marker} zoom={15}>
-            <TileLayer
+            <TileLayer //@ts-ignore
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />

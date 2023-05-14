99d59cfb98ecb73af00bed53c6725daba0e11fed
gabrielTor <gabriel_torres2012@yahoo.com>
2023-05-13 23:32:37 -0300

    skeleton loading

--- pages/galeria.tsx
+++ pages/galeria.tsx
@@ -5,19 +5,26 @@ import styles from '../styles/Gallery.module.css'
 import Image from 'next/image'
 
 const loadUrl = ({ src, width }: { src: string, width: number }) => `${src}?s=${width}`
+const loaderArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 
 const Galeria: NextPage = () => {
     const [urls, setUrls] = useState<(string | Promise<string>)[]>([])
+    const [isLoading, setIsLoading] = useState<boolean>(true)
     let mounted = true
 
     useEffect(() => {
-        if (mounted) getImages(setUrls)
+        if (mounted){
+            getImages(setUrls)
+            setTimeout(() => setIsLoading(false), 2000)
+        }
         return () => { mounted = false }
     }, [])
 
     return (
         <div className={styles.container}>
-            {
+            {isLoading ?
+                loaderArray.map((a) => <div key={a} className={styles.loader} />)
+                :
                 urls?.map((src) => (
                     <Image key={src as string}
                         loader={loadUrl}
